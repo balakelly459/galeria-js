d32be3fd2bc917c334ec26bea727f0cd04daaa9c
balakelly459 <21192911@CFP127ALUNO.LOCAL>
2021-09-21 10:46:03 -0300

    js

---
+++ galeria.js
@@ -0,0 +1,30 @@
+"use strict"
+
+const imagens = [
+    "./img/img1.jpg",
+    "./img/img2.jpg",
+    "./img/img3.jpg",
+    "./img/img4.jpg",
+    "./img/img5.jpg",
+    "./img/img6.jpg",
+    "./img/img7.jpg",
+    "./img/img8.png"
+]
+
+const criarItem = (urlImagem) => {
+    const container = document.querySelector(".galeria-container")
+    // container.innerHTML += `
+    // <a href="${urlImagem}" class="galeria-itens">
+    // <img src="${urlImagem}" alt="">
+    // </a>
+    // `
+    const novoLink = document.createElement("a")
+    novoLink.href = urlImagem
+    novoLink.classList.add("galeria-itens")
+    novoLink.innerHTML = `<img src="${urlImagem}" alt="">`
+    container.appendChild(novoLink)
+}
+
+const carregarImagens = () => imagens.forEach(criarItem)
+
+carregarImagens()
