8b22a0f0d64cd1276112e39eb614550ec3051c3c
balakelly459 <21192911@CFP127ALUNO.LOCAL>
2021-09-28 10:35:09 -0300

    slides

--- galeria.js
+++ galeria.js
@@ -1,15 +1,15 @@
 "use strict"
 
-const imagens = [
-    "./img/img1.jpg",
-    "./img/img2.jpg",
-    "./img/img3.jpg",
-    "./img/img4.jpg",
-    "./img/img5.jpg",
-    "./img/img6.jpg",
-    "./img/img7.jpg",
-    "./img/img8.png"
-]
+// const imagens = [
+//     "./img/img1.jpg",
+//     "./img/img2.jpg",
+//     "./img/img3.jpg",
+//     "./img/img4.jpg",
+//     "./img/img5.jpg",
+//     "./img/img6.jpg",
+//     "./img/img7.jpg",
+//     "./img/img8.png"
+// ]
 
 const criarItem = (urlImagem) => {
     const container = document.querySelector(".galeria-container")
